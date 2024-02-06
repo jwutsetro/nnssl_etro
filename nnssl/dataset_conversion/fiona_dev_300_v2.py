b7c18ad7b66769038a745088950f6e6b72e9d5ce
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-02-06 15:27:25 +0100

    Add nnunet_raw_dataset_creation

---
+++ nnssl/dataset_conversion/fiona_dev_300_v2.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import shutil
+import valohai
+from batchgenerators.utilities.file_and_folder_operations import save_json
+
+
+if __name__ == "__main__":
+    train_data_path = Path(valohai.inputs("raw-data"))
+
+    dataset_json = {
+        "channel_names": {"0": "someMRI"},
+        "file_ending": ".nii.gz",
+        "numTraining": len([sdf for sdf in train_data_path.iterdir() if sdf.name.endswith("nii.gz")]),
+        "name": "Cases of Floys initial Dataset",
+        "release": "0.0",
+        "licence": "Proprietary -- do not touch without permission",
+        "description": "Unlabeled set of datapoints that are used for pre-text task pretraining",
+    }
+    meta_data_json = {"valohai.dataset-versions": ["dataset://fiona-300-dev/v2"]}
+    for sdf in train_data_path.iterdir():
+        sdf_name = sdf.name
+        if sdf_name.endswith("nii.gz"):
+            shutil.copy(sdf, Path("/valohai/outputs" / sdf_name))
+            save_json(meta_data_json, Path("/valohai/outputs" / (sdf_name + ".metadata.json")))
+
+    print("Done!")
