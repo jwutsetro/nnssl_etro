d7718a0a6bb7b4b6890a9c8982ba5f1b09d9c0f5
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-03-13 15:31:19 +0100

    Add valohai_requests.py to keep all the API calls nicely tied together

---
+++ nnssl/scripts/valohai_requests.py
@@ -0,0 +1,128 @@
+from nnssl.apitoken import valohai_api_token
+import requests
+
+
+def get_valohai_projects() -> dict:
+    headers = get_auth_header()
+    # ------------------------------- Get Projects ------------------------------- #
+    resp = requests.get("https://app.valohai.com/api/v0/projects/", headers=headers)
+    projects = {}
+    for r in resp.json()["results"]:
+        projects[r["name"]] = r
+    return projects
+
+
+def get_valohai_stores() -> dict:
+    # ------------------------------- Get Stores ------------------------------- #
+    store_ids = requests.get(url="https://app.valohai.com/api/v0/stores/", headers=get_auth_header())
+    stores = {}
+    for s in store_ids.json()["results"]:
+        stores[s["name"]] = s
+    return stores
+
+
+def get_andrei_adopt_output():
+    """Gets the outputs of the execution with the ID 018e1622-540c-4ed6-ed44-72c3194758a7."""
+    return get_execution_output("018e1622-540c-4ed6-ed44-72c3194758a7")
+
+
+def get_auth_header() -> dict[str, str]:
+    return {"Authorization": "Token %s" % valohai_api_token}
+
+
+def get_execution_output(execution_id):
+    url = f"https://app.valohai.com/api/v0/executions/{execution_id}/outputs/"
+    header = get_auth_header()
+    response = requests.get(url=url, headers=header)
+    response_json = response.json()
+    return response_json
+
+
+def get_andrei_adopt_output():
+    """Gets the outputs of the execution with the ID 018e1622-540c-4ed6-ed44-72c3194758a7."""
+    return get_execution_output("018e1622-540c-4ed6-ed44-72c3194758a7")
+
+
+def check_for_datum_uuid(query: dict) -> str | None:
+    """Takes some infos about a file and checks if it's already in the Valohai dataset."""
+    header = {
+        "Authorization": "Token %s" % valohai_api_token,
+    }
+    apendix = ""
+    for cnt, (k, v) in enumerate(query.items()):
+        if cnt == 0:
+            apendix = f"?{k}={v}"
+        else:
+            apendix += f"&{k}={v}"
+    query_url = f"https://app.valohai.com/api/v0/data/{apendix}"
+    # query_url = f"https://app.valohai.com/api/v0/data/?name={name}&store={store}&project={project_id}"
+
+    response = requests.get(url=query_url, headers=header)
+    response_json = response.json()
+    if response_json["count"] == 0:
+        return None
+    else:
+        return response_json["results"][0]["id"]
+
+
+def get_dataset_uid_by_name(name: str) -> str | None:
+    """Gets a dataset_uid by name. Returns None if it doesn't exist."""
+    headers = get_auth_header()
+    existing_datasets = requests.get("https://app.valohai.com/api/v0/datasets/", headers=headers).json()
+
+    for ds in existing_datasets["results"]:
+        if ds["name"] == name:
+            return ds["id"]
+    return
+
+
+def maybe_create_new_valohai_dataset(dataset_name: str, owner: int = 0) -> str:
+    """
+    Checks is the dataset_name is already in valohai.
+    If not creates it and returns the `datum_id` of it.
+    """
+
+    # Check users to make sure we set it to the right one.
+    # headers = get_auth_header_token()
+    # usrs = requests.get("https://app.valohai.com/api/v0/users/", headers=headers).json()
+    # orgs = requests.get("https://app.valohai.com/api/v0/organizations/", headers=headers).json()
+    # Manually check which one Floy is.
+    ds_uid = get_dataset_uid_by_name(dataset_name)
+    if ds_uid is not None:
+        return ds_uid
+    # --------------------- If not exists we create a new one -------------------- #
+    post_url = f"https://app.valohai.com/api/v0/datasets/"
+    post_request_body = {
+        "name": dataset_name,
+        "owner": owner,
+    }
+    response = requests.post(post_url, post_request_body, headers=get_auth_header()).json()
+    ds_uid = response["id"]
+    return ds_uid
+
+
+def maybe_create_new_dataset_version(
+    dataset_name: str, version: str, files: list[dict[str, str]], owner: int
+) -> dict:
+    """
+    Creates a new version of a dataset.
+    """
+    ds_id = maybe_create_new_valohai_dataset(dataset_name, owner)
+
+    post_url = f"https://app.valohai.com/api/v0/dataset-versions/"
+    post_request_body = {
+        "name": version,
+        "dataset": ds_id,
+        "files": files,
+    }
+    response = requests.post(post_url, json=post_request_body, headers=get_auth_header())
+    return response
+
+
+def convert_andrei_adtop_to_lookup(andrei_adopt_output: list[dict]) -> dict[str, dict]:
+    """Converts the output of get_andrei_adopt_output to a lookup table."""
+    lookup = {}
+    for pat in andrei_adopt_output:
+        lookup_key = pat["name"].split("/")[-1].split(".")[0]
+        lookup[lookup_key] = pat
+    return lookup
