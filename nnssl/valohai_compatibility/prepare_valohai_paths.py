31b9cc91196e56c743eac20177dfa0bf7e94ae7a
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-03-13 23:33:36 +0100

    Fix removal of "/" symbol

--- nnssl/valohai_compatibility/prepare_valohai_paths.py
+++ nnssl/valohai_compatibility/prepare_valohai_paths.py
@@ -115,6 +115,8 @@ def prepare_preprocessing_paths_on_valohai(dataset_id: int):
                 continue
             if "/" in f:
                 f_target = f.split("/")[-1]
+            else:
+                f_target = f
             shutil.copy(os.path.join(flat_inputs, f), os.path.join(nnunet_raw_dataset_imgs, f_target))
         print("Found", len(not_3d_files), "files that are not 3D. Ignoring them.")
         print(f"Moved {len(os.listdir(nnunet_raw_dataset_imgs))} files to {nnunet_raw_dataset_imgs}")
