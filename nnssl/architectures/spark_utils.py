1a5dd7a90b93afd277f031685950b72af4c457b3
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-06-19 05:26:41 +0200

    Add two versions of SparkMAEs one with and one without einops stuff

--- nnssl/architectures/spark_utils.py
+++ nnssl/architectures/spark_utils.py
@@ -102,30 +102,24 @@ def sp_bn_forward(self, x: torch.Tensor):
     mask = _get_active_ex_or_ii(
         B=x.shape[0], D=x.shape[2], H=x.shape[3], W=x.shape[4], device=x.device, dtype=x.dtype
     )
-    # active_ex.squeeze(1).nonzero(as_tuple=True)  # ii: bi, di, hi, wi
-    #   Should normalize by sample now (not by batch, as we do instance norm and not batchnorm!)
-    # x_pre_in = rearrange(x, "b c d h w -> b d h w c")
 
     x_pre_in = torch.permute(x, (0, 2, 3, 4, 1))
     mask = torch.squeeze(mask, 1)
     L = int(torch.sum(mask, dim=(1, 2, 3))[0])
     mask_ids = torch.nonzero(mask, as_tuple=True)
     flat_values = x_pre_in[mask_ids]
-    # ncl = rearrange(flat_values, "(b L) c -> b c L", b=x.shape[0], c=x.shape[1], L=int(L))  # (BCL) -> (BCL)
+
     pre_nlc = torch.reshape(flat_values, (B, L, C))  # (BL)C -> BLC
     pre_ncl = torch.permute(pre_nlc, (0, 2, 1))  # BLC -> BCL
     post_ncl = super(type(self), self).forward(pre_ncl)  # use BN1d to normalize this flatten feature `nc`
-    # ncl = rearrange(ncl, "b c L -> (b L) c")  # (BCL) -> (BCL)
+
     post_nlc = torch.permute(post_ncl, (0, 2, 1))  # BCL -> BLC
     post_ncl = torch.reshape(post_nlc, (B * L, C))  # BLC -> (BL)C
 
     x_postin = torch.zeros_like(x_pre_in, dtype=x_pre_in.dtype, device=x_pre_in.device)
     x_postin[mask_ids] = post_ncl
-    # x_postin = rearrange(x_postin, "b d h w c -> b c d h w")  # (BDHWC) -> (BCDHW)
+
     x_postin = torch.permute(x_postin, (0, 4, 1, 2, 3))
-    # bcdhw = rearrange(
-    #     x_postbn, "b c (d h w)  -> b c d h w", d=x.shape[2], h=x.shape[3], w=x.shape[4]
-    # )  # reshape the normalized flatten feature back to the original shape
     return x_postin
 
 
@@ -141,51 +135,6 @@ class SparseAvgPooling(nn.AvgPool3d):
     forward = sp_conv_forward  # hack: override the forward function; see `sp_conv_forward` above for more details
 
 
-class SparseBatchNorm3d(nn.BatchNorm1d):
-    forward = sp_bn_forward  # hack: override the forward function; see `sp_bn_forward` above for more details
-
-
-# class SparseInstanceNorm3d(nn.InstanceNorm3d):
-
-#     def _apply_instance_norm(self, input):
-#         with FakeTensorMode(allow_non_fake_inputs=True):
-#             x = input
-#             mask = _get_active_ex_or_ii(
-#                 B=x.shape[0], D=x.shape[2], H=x.shape[3], W=x.shape[4], device=x.device, dtype=x.dtype
-#             )
-#             n_active = torch.sum(mask, dim=(1, 2, 3, 4), keepdim=True)  # [B, 1, 1, 1, 1]
-#             foreground_sum = torch.sum(x * mask, dim=(2, 3, 4), keepdim=True)  # [B, C, 1, 1, 1]
-#             foreground_mean = foreground_sum / n_active  # B C 1 1 1
-#             foreground_var = torch.sum(((x - foreground_mean) ** 2) * mask, dim=(2, 3, 4), keepdim=True) / n_active
-
-#             x_instance_normed = (x - foreground_mean) / torch.sqrt(foreground_var + self.eps)
-
-#             return (
-#                 x_instance_normed * self.weight[None, :, None, None, None] + self.bias[None, :, None, None, None]
-#             ) * mask
-
-# def forward(self, x: torch.Tensor):
-#     # mask : B 1 D H W
-
-#     if self.momentum is None:
-#         exponential_average_factor = 0.0
-#     else:
-#         exponential_average_factor = self.momentum
-
-#     if self.training and self.track_running_stats:
-#         if self.num_batches_tracked is not None:  # type: ignore[has-type]
-#             self.num_batches_tracked.add_(1)  # type: ignore[has-type]
-#             if self.momentum is None:  # use cumulative moving average
-#                 exponential_average_factor = 1.0 / float(self.num_batches_tracked)
-#             else:  # use exponential moving average
-#                 exponential_average_factor = self.momentum
-
-#     if self.training:
-#         bn_training = True
-#     else:
-#         bn_training = (self.running_mean is None) and (self.running_var is None)
-
-
 class einops_SparseInstanceNorm3d(nn.InstanceNorm1d):
     forward = einops_sp_bn_forward  # hack: override the forward function; see `sp_bn_forward` above for more details
 
@@ -198,86 +147,86 @@ class SparseSyncBatchNorm3d(nn.SyncBatchNorm):
     forward = sp_bn_forward  # hack: override the forward function; see `sp_bn_forward` above for more details
 
 
-def convert_to_spark_cnn(m: nn.Module, verbose=False, sbn=False):
-    oup = m
-    if isinstance(m, nn.Conv3d):
-        m: nn.Conv3d
-        bias = m.bias is not None
-        oup = SparseConv3d(
-            m.in_channels,
-            m.out_channels,
-            kernel_size=m.kernel_size,
-            stride=m.stride,
-            padding=m.padding,
-            dilation=m.dilation,
-            groups=m.groups,
-            bias=bias,
-            padding_mode=m.padding_mode,
-        )
-        oup.weight.data.copy_(m.weight.data)
-        if bias:
-            oup.bias.data.copy_(m.bias.data)
-    elif isinstance(m, nn.MaxPool3d):
-        m: nn.MaxPool3d
-        oup = SparseMaxPooling(
-            m.kernel_size,
-            stride=m.stride,
-            padding=m.padding,
-            dilation=m.dilation,
-            return_indices=m.return_indices,
-            ceil_mode=m.ceil_mode,
-        )
-    elif isinstance(m, nn.AvgPool3d):
-        m: nn.AvgPool3d
-        oup = SparseAvgPooling(
-            m.kernel_size,
-            m.stride,
-            m.padding,
-            ceil_mode=m.ceil_mode,
-            count_include_pad=m.count_include_pad,
-            divisor_override=m.divisor_override,
-        )
-    elif isinstance(m, (nn.BatchNorm3d, nn.SyncBatchNorm)):
-        m: nn.BatchNorm3d
-        oup = (SparseSyncBatchNorm3d if sbn else SparseBatchNorm3d)(
-            m.weight.shape[0],
-            eps=m.eps,
-            momentum=m.momentum,
-            affine=m.affine,
-            track_running_stats=m.track_running_stats,
-        )
-        oup.weight.data.copy_(m.weight.data)
-        oup.bias.data.copy_(m.bias.data)
-        oup.running_mean.data.copy_(m.running_mean.data)
-        oup.running_var.data.copy_(m.running_var.data)
-        oup.num_batches_tracked.data.copy_(m.num_batches_tracked.data)
-        if hasattr(m, "qconfig"):
-            oup.qconfig = m.qconfig
-    elif isinstance(m, (nn.InstanceNorm3d,)):
-        m: nn.InstanceNorm3d
-        oup = SparseInstanceNorm3d(
-            m.weight.shape[0],
-            eps=m.eps,
-            momentum=m.momentum,
-            affine=m.affine,
-            track_running_stats=m.track_running_stats,
-        )
-        oup.weight.data.copy_(m.weight.data)
-        oup.bias.data.copy_(m.bias.data)
-        if hasattr(m, "qconfig"):
-            oup.qconfig = m.qconfig
-    # elif isinstance(m, nn.LayerNorm) and not isinstance(m, SparseConvNeXtLayerNorm):
-    #     m: nn.LayerNorm
-    #     oup = SparseConvNeXtLayerNorm(m.weight.shape[0], eps=m.eps)
-    #     oup.weight.data.copy_(m.weight.data)
-    #     oup.bias.data.copy_(m.bias.data)
-    elif isinstance(m, (nn.Conv1d,)):
-        raise NotImplementedError
-    # Right now seems a bit fishy. Seems like infinite recursion.
-    for name, child in m.named_children():
-        oup.add_module(name, convert_to_spark_cnn(child, verbose=verbose, sbn=sbn))
-    del m
-    return oup
+# def convert_to_spark_cnn(m: nn.Module, verbose=False, sbn=False):
+#     oup = m
+#     if isinstance(m, nn.Conv3d):
+#         m: nn.Conv3d
+#         bias = m.bias is not None
+#         oup = SparseConv3d(
+#             m.in_channels,
+#             m.out_channels,
+#             kernel_size=m.kernel_size,
+#             stride=m.stride,
+#             padding=m.padding,
+#             dilation=m.dilation,
+#             groups=m.groups,
+#             bias=bias,
+#             padding_mode=m.padding_mode,
+#         )
+#         oup.weight.data.copy_(m.weight.data)
+#         if bias:
+#             oup.bias.data.copy_(m.bias.data)
+#     elif isinstance(m, nn.MaxPool3d):
+#         m: nn.MaxPool3d
+#         oup = SparseMaxPooling(
+#             m.kernel_size,
+#             stride=m.stride,
+#             padding=m.padding,
+#             dilation=m.dilation,
+#             return_indices=m.return_indices,
+#             ceil_mode=m.ceil_mode,
+#         )
+#     elif isinstance(m, nn.AvgPool3d):
+#         m: nn.AvgPool3d
+#         oup = SparseAvgPooling(
+#             m.kernel_size,
+#             m.stride,
+#             m.padding,
+#             ceil_mode=m.ceil_mode,
+#             count_include_pad=m.count_include_pad,
+#             divisor_override=m.divisor_override,
+#         )
+#     elif isinstance(m, (nn.BatchNorm3d, nn.SyncBatchNorm)):
+#         m: nn.BatchNorm3d
+#         oup = (SparseSyncBatchNorm3d if sbn else SparseBatchNorm3d)(
+#             m.weight.shape[0],
+#             eps=m.eps,
+#             momentum=m.momentum,
+#             affine=m.affine,
+#             track_running_stats=m.track_running_stats,
+#         )
+#         oup.weight.data.copy_(m.weight.data)
+#         oup.bias.data.copy_(m.bias.data)
+#         oup.running_mean.data.copy_(m.running_mean.data)
+#         oup.running_var.data.copy_(m.running_var.data)
+#         oup.num_batches_tracked.data.copy_(m.num_batches_tracked.data)
+#         if hasattr(m, "qconfig"):
+#             oup.qconfig = m.qconfig
+#     elif isinstance(m, (nn.InstanceNorm3d,)):
+#         m: nn.InstanceNorm3d
+#         oup = SparseInstanceNorm3d(
+#             m.weight.shape[0],
+#             eps=m.eps,
+#             momentum=m.momentum,
+#             affine=m.affine,
+#             track_running_stats=m.track_running_stats,
+#         )
+#         oup.weight.data.copy_(m.weight.data)
+#         oup.bias.data.copy_(m.bias.data)
+#         if hasattr(m, "qconfig"):
+#             oup.qconfig = m.qconfig
+#     # elif isinstance(m, nn.LayerNorm) and not isinstance(m, SparseConvNeXtLayerNorm):
+#     #     m: nn.LayerNorm
+#     #     oup = SparseConvNeXtLayerNorm(m.weight.shape[0], eps=m.eps)
+#     #     oup.weight.data.copy_(m.weight.data)
+#     #     oup.bias.data.copy_(m.bias.data)
+#     elif isinstance(m, (nn.Conv1d,)):
+#         raise NotImplementedError
+#     # Right now seems a bit fishy. Seems like infinite recursion.
+#     for name, child in m.named_children():
+#         oup.add_module(name, convert_to_spark_cnn(child, verbose=verbose, sbn=sbn))
+#     del m
+#     return oup
 
 
 def convert_to_spark_cnn(m: nn.Module, verbose=False, sbn=False):
@@ -321,21 +270,7 @@ def convert_to_spark_cnn(m: nn.Module, verbose=False, sbn=False):
             divisor_override=m.divisor_override,
         )
     elif isinstance(m, (nn.BatchNorm3d, nn.SyncBatchNorm)):
-        m: nn.BatchNorm3d
-        oup = (SparseSyncBatchNorm3d if sbn else SparseBatchNorm3d)(
-            m.weight.shape[0],
-            eps=m.eps,
-            momentum=m.momentum,
-            affine=m.affine,
-            track_running_stats=m.track_running_stats,
-        )
-        oup.weight.data.copy_(m.weight.data)
-        oup.bias.data.copy_(m.bias.data)
-        oup.running_mean.data.copy_(m.running_mean.data)
-        oup.running_var.data.copy_(m.running_var.data)
-        oup.num_batches_tracked.data.copy_(m.num_batches_tracked.data)
-        if hasattr(m, "qconfig"):
-            oup.qconfig = m.qconfig
+        raise NotImplementedError("Only InstanceNorm supported atm.")
     elif isinstance(m, (nn.InstanceNorm3d,)):
         m: nn.InstanceNorm3d
         oup = SparseInstanceNorm3d(
@@ -354,6 +289,68 @@ def convert_to_spark_cnn(m: nn.Module, verbose=False, sbn=False):
     # Right now seems a bit fishy. Seems like infinite recursion.
     for name, child in m.named_children():
         oup.add_module(name, convert_to_spark_cnn(child, verbose=verbose, sbn=sbn))
+    del m
+    return oup
+
+
+def convert_to_einops_spark_cnn(m: nn.Module, verbose=False, sbn=False):
+    # Dummy to see if this is what breaks torch.compile or if it's something else.
+    oup = m
+    if isinstance(m, nn.Conv3d):
+        m: nn.Conv3d
+        bias = m.bias is not None
+        oup = SparseConv3d(
+            m.in_channels,
+            m.out_channels,
+            kernel_size=m.kernel_size,
+            stride=m.stride,
+            padding=m.padding,
+            dilation=m.dilation,
+            groups=m.groups,
+            bias=bias,
+            padding_mode=m.padding_mode,
+        )
+        oup.weight.data.copy_(m.weight.data)
+        if bias:
+            oup.bias.data.copy_(m.bias.data)
+    elif isinstance(m, nn.MaxPool3d):
+        m: nn.MaxPool3d
+        oup = SparseMaxPooling(
+            m.kernel_size,
+            stride=m.stride,
+            padding=m.padding,
+            dilation=m.dilation,
+            return_indices=m.return_indices,
+            ceil_mode=m.ceil_mode,
+        )
+    elif isinstance(m, nn.AvgPool3d):
+        m: nn.AvgPool3d
+        oup = SparseAvgPooling(
+            m.kernel_size,
+            m.stride,
+            m.padding,
+            ceil_mode=m.ceil_mode,
+            count_include_pad=m.count_include_pad,
+            divisor_override=m.divisor_override,
+        )
+    elif isinstance(m, (nn.InstanceNorm3d,)):
+        m: nn.InstanceNorm3d
+        oup = einops_SparseInstanceNorm3d(
+            m.weight.shape[0],
+            eps=m.eps,
+            momentum=m.momentum,
+            affine=m.affine,
+            track_running_stats=m.track_running_stats,
+        )
+        oup.weight.data.copy_(m.weight.data)
+        oup.bias.data.copy_(m.bias.data)
+        if hasattr(m, "qconfig"):
+            oup.qconfig = m.qconfig
+    elif isinstance(m, (nn.Conv1d,)):
+        raise NotImplementedError
+    # Right now seems a bit fishy. Seems like infinite recursion.
+    for name, child in m.named_children():
+        oup.add_module(name, convert_to_einops_spark_cnn(child, verbose=verbose, sbn=sbn))
     del m
     return oup
 
@@ -384,10 +381,13 @@ if __name__ == "__main__":
     x = torch.randn(4, 3, 16, 16, 16)
 
     conv = torch.compile(sparse_conv, options={"trace.enabled": True})
+    conv(x)
     print("Compiled conv")
     out_a = torch.compile(sp_in)
+    out_a(x)
     print("Compiled IN")
     joint = torch.nn.Sequential(*[sparse_conv, sp_in])
-    torch.compile(joint)
+    joint_c = torch.compile(joint)
+    joint_c(x)
     print("Compiled joint")
     sys.exit(0)
---
+++ nnssl/training/nnsslTrainer/SparkEinOpsTrainer.py
@@ -0,0 +1,66 @@
+from typing import Any
+
+from torch import device
+from torch import nn
+from torch._C import device
+from nnssl.architectures.spark_model import SparK3D
+from nnssl.architectures.spark_utils import convert_to_spark_cnn
+from nnssl.experiment_planning.experiment_planners.plan import Plan
+from nnssl.training.nnsslTrainer.SparkTrainer import SparkMAETrainer
+from dynamic_network_architectures.architectures.unet import ResidualEncoderUNet
+
+
+class EinOps_SparkMAETrainer(SparkMAETrainer):
+
+    def __init__(
+        self,
+        plan: Plan,
+        configuration_name: str,
+        fold: int,
+        dataset_json: dict,
+        unpack_dataset: bool = True,
+        device: device = ...,
+    ):
+        super().__init__(plan, configuration_name, fold, dataset_json, unpack_dataset, device)
+
+    def build_architecture(self, *args, **kwargs) -> nn.Module:
+        n_stages = 6
+        network = ResidualEncoderUNet(
+            input_channels=1,
+            n_stages=n_stages,
+            features_per_stage=[32, 64, 128, 256, 320, 320],
+            conv_op=nn.Conv3d,
+            kernel_sizes=[[3, 3, 3] for _ in range(n_stages)],
+            strides=[[1, 1, 1], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2], [2, 2, 2]],
+            n_blocks_per_stage=[1, 3, 4, 6, 6, 6],
+            num_classes=1,
+            n_conv_per_stage_decoder=[1, 1, 1, 1, 1],
+            conv_bias=True,
+            norm_op=nn.InstanceNorm3d,
+            norm_op_kwargs={"eps": 1e-5, "affine": True},
+            nonlin=nn.LeakyReLU,
+            nonlin_kwargs={"inplace": True},
+            deep_supervision=False,
+        )
+
+        spark_architecture = convert_to_spark_cnn(network.encoder)
+        network.encoder = spark_architecture
+        actual_network = SparK3D(network, (160, 160, 160), self.use_mask_token)
+
+        return actual_network
+
+
+class EinOps_SparkMAETrainer_5ep_BS6(EinOps_SparkMAETrainer):
+
+    def __init__(
+        self,
+        plan: Plan,
+        configuration_name: str,
+        fold: int,
+        dataset_json: dict,
+        unpack_dataset: bool = True,
+        device: device = ...,
+    ):
+        plan.configurations[configuration_name].batch_size = 6
+        self.num_epochs = 5
+        super().__init__(plan, configuration_name, fold, dataset_json, unpack_dataset, device)
