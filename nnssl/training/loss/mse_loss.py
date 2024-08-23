5e6146eb0ccd311a4e2e9ca529d8dc131c0d99c5
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-08-23 08:08:17 +0200

    Fix BaseMAE loss

--- nnssl/training/loss/mse_loss.py
+++ nnssl/training/loss/mse_loss.py
@@ -12,10 +12,8 @@ class MAEMSELoss(AbstractLoss):
         self, model_output: torch.Tensor, target: dict[str, torch.Tensor], mask: torch.Tensor
     ) -> torch.Tensor:
         """Can take any outputs,  ."""
-        reconstruction = target["target"]
-        mask = target["mask"]
         # Mask = 1 represents not masked points
-        reconstruction_loss = (model_output - reconstruction) ** 2  # (B, X, Y, Z, C)
-        reconstruction_loss = torch.sum(reconstruction_loss * mask) / torch.sum(mask)
+        reconstruction_loss = (model_output - target) ** 2  # (B, X, Y, Z, C)
+        reconstruction_loss = torch.sum(reconstruction_loss * (1 - mask)) / torch.sum((1 - mask))
 
         return reconstruction_loss
