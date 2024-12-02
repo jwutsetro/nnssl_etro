fd1e6554a723e90a4d71fd072e5541448a22937a
j385i <jonathan.suprijadi@dkfz-heidelberg.de>
2024-12-02 13:14:22 +0100

    deleted dataset.json in BaseMAETrainer_BS6 init

--- src/nnssl/training/nnsslTrainer/masked_image_modeling/BaseMAETrainer.py
+++ src/nnssl/training/nnsslTrainer/masked_image_modeling/BaseMAETrainer.py
@@ -455,9 +455,8 @@ class BaseMAETrainer_BS6(BaseMAETrainer):
         plan: Plan,
         configuration_name: str,
         fold: int,
-        dataset_json: dict,
         pretrain_json: dict,
         device: torch.device = torch.device("cuda"),
     ):
         plan.configurations[configuration_name].batch_size = 6
-        super().__init__(plan, configuration_name, fold, dataset_json, pretrain_json, device)
+        super().__init__(plan, configuration_name, fold, pretrain_json, device)
