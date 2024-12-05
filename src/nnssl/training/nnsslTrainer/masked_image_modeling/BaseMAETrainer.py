72a40abc8e62d394ecb2d66e867611bb94f53054
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-12-05 14:04:07 +0100

    Create small trainer to verify auto-restart

--- src/nnssl/training/nnsslTrainer/masked_image_modeling/BaseMAETrainer.py
+++ src/nnssl/training/nnsslTrainer/masked_image_modeling/BaseMAETrainer.py
@@ -517,3 +517,17 @@ class BaseMAETrainer_BS8_5ep(BaseMAETrainer):
         plan.configurations[configuration_name].batch_size = 8
         super().__init__(plan, configuration_name, fold, pretrain_json, device)
         self.num_epochs = 5
+
+
+class BaseMAETrainer_BS8_100ep(BaseMAETrainer):
+    def __init__(
+        self,
+        plan: Plan,
+        configuration_name: str,
+        fold: int,
+        pretrain_json: dict,
+        device: torch.device = torch.device("cuda"),
+    ):
+        plan.configurations[configuration_name].batch_size = 8
+        super().__init__(plan, configuration_name, fold, pretrain_json, device)
+        self.num_epochs = 100
