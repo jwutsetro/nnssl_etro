7722ad20724d24c2bc4a2dbcc0dce3cf1aa17e58
d246a <d.trofimova@dkfz-heidelberg.de>
2024-12-05 11:53:22 +0100

    f

--- src/nnssl/run/run_training.py
+++ src/nnssl/run/run_training.py
@@ -92,6 +92,7 @@ def get_trainer_from_args(
         pretrain_json,
         device,
         *args,
+        **kwargs
     )
     return nnssl_trainer
 
@@ -196,7 +197,7 @@ def run_ddp(
     #torch.cuda.set_device(torch.device("cuda", dist.get_rank()))
  
     device = torch.device(f"cuda:{rank}")
-    nnunet_trainer = get_trainer_from_args(dataset_name_or_id, configuration, fold, tr, p, device= device, *args)
+    nnunet_trainer = get_trainer_from_args(dataset_name_or_id, configuration, fold, tr, p, device= device, *args, **kwargs)
     if disable_checkpointing:
         nnunet_trainer.disable_checkpointing = disable_checkpointing
 
