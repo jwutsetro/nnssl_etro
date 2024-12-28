9d25e4b177a4b21c4787f84af6fd8f46a2fb04ea
Wald, Tassilo <tassilo.wald@dkfz-heidelberg.de>
2024-12-28 17:21:57 +0100

    Add bigger maskblock. Make 64 now

--- src/nnssl/training/nnsslTrainer/masked_image_modeling/base_mae_derivates/var_mask_sizes.py
+++ src/nnssl/training/nnsslTrainer/masked_image_modeling/base_mae_derivates/var_mask_sizes.py
@@ -90,3 +90,25 @@ class BaseMAETrainer_BS8_ep1000_maskblock32(BaseMAETrainer_BS8_1000ep):
         mask = [create_blocky_mask(patch_size, block_size, sparsity_factor) for _ in range(batch_size)]
         mask = torch.stack(mask)[:, None, ...]  # Add channel dimension
         return mask
+
+
+class BaseMAETrainer_BS8_ep1000_maskblock64(BaseMAETrainer_BS8_1000ep):
+    @staticmethod
+    def mask_creation(
+        batch_size: int, patch_size: tuple[int, int, int], mask_percentage: float, rng_seed: int | None = None
+    ) -> torch.Tensor:
+        """
+        Creates a masking tensor with 1s (indicating no masking) and 0s (indicating masking).
+        The mask has to be of same size like the input data (batch_size, 1, x, y, z).
+
+        :param patch_shape: The 3D shape information for the masking patch.
+        :param mask_percentage: percentage of the patch that should be masked
+        :param min_mask_block_size: minimum size of the blocks that should be masked
+        :return:
+        """
+
+        block_size = 64
+        sparsity_factor = mask_percentage
+        mask = [create_blocky_mask(patch_size, block_size, sparsity_factor) for _ in range(batch_size)]
+        mask = torch.stack(mask)[:, None, ...]  # Add channel dimension
+        return mask
